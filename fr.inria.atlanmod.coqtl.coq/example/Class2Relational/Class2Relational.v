8aa907cf85eefb7f01ee0e35dd6b8ec357575729
Zheng Cheng <cz0590@gmail.com>
2018-06-01 14:34:08 +0200

    add expected class2rel transformation def

--- fr.inria.atlanmod.coqtl.coq/example/Class2Relational/Class2Relational.v
+++ fr.inria.atlanmod.coqtl.coq/example/Class2Relational/Class2Relational.v
@@ -12,87 +12,40 @@ Require Import example.ClassMetamodel.
 Require Import example.RelationalMetamodel.
 
 Definition Class2Relational :=
-  transformation Class2Relational from ClassMetamodel to RelationalMetamodel
-    with m as ClassModel := [
+  (BuildTransformation ClassMetamodel RelationalMetamodel
+  [
+     (BuildRule [ClassEClass]
+        (fun (m: ClassModel) (c: Class) => true)
+        [
+        (BuildOutputPatternElement "tab" TableClass
+            (fun (m: ClassModel) (c: Class) => BuildTable (getClassId c) (getClassName c))
+            [
+                (BuildOutputPatternElementReference TableColumnReference
+                    (fun (m: ClassModel) (c: Class) (t: Table) (Class2Relational: Transformation) =>
+                        reference TableColumnsReference from RelationalMetamodel :=
+                        attrs <- getClassAttributes c m;
+                        cols <- resolveAll Class2Relational m "col" ColumnClass
+                            (singletons (map (A:=Attribute) ClassMetamodel_toEObject attrs));
+                        return BuildTableColumns t cols))
+            ])                               
+        ]);
+     (BuildRule [AttributeEClass]
+        (fun (m: ClassModel) (a: Attribute) => true)
+        [
+        (BuildOutputPatternElement "col" ColumnClass
+            (fun (m: ClassModel) (a: Attribute) => BuildColumn (getAttributeId a) (getAttributeName a))
+            [
+                (BuildOutputPatternElementReference ColumnReferenceReference
+                    (fun (m: ClassModel) (a: Attribute) (c: Column) (Class2Relational: Transformation) =>
+                        reference ColumnReferenceReference from RelationalMetamodel :=
+                            cl <- getAttributeType a m;
+                            tb <- resolve Class2Relational m "tab" TableClass [ClassMetamodel_toEObject cl];
+                            return BuildColumnReference c tb))
+            ])                               
+        ])
+  ]).
 
-       rule Class2Table
-         from
-           element c class ClassEClass from ClassMetamodel
-             when true
-         to
-          [
-           output "tab"
-             element t class TableClass from RelationalMetamodel :=
-               BuildTable (getClassId c) (getClassName c)
-             links
-               [
-                 reference TableColumnsReference from RelationalMetamodel :=
-                   attrs <- getClassAttributes c m;
-                   cols <- resolveAll Class2Relational m "col" ColumnClass
-                      (singletons (map (A:=Attribute) ClassMetamodel_toEObject attrs));
-                   return BuildTableColumns t cols
-               ]
-          ];
+Unset Printing Notations.
 
-      rule Attribute2Column
-        from
-          element a class AttributeEClass from ClassMetamodel 
-            when (negb (getAttributeDerived a))
-        to
-         [
-          output "col"
-            element c class ColumnClass from RelationalMetamodel := 
-               BuildColumn (getAttributeId a) (getAttributeName a)
-            links
-              [
-                reference ColumnReferenceReference from RelationalMetamodel :=
-                  cl <- getAttributeType a m;
-                  tb <- resolve Class2Relational m "tab" TableClass [ClassMetamodel_toEObject cl];
-                  return BuildColumnReference c tb
-              ] 
-         ]
-
-  ].
-
-(*Unset Printing Notations.*)
-Print Class2Relational.
-
-Theorem All_classes_match :
-  forall (cm : ClassModel) (c: Class) ,
-    exists (r: RuleA ClassMetamodel_EClass RelationalMetamodel_EClass RelationalMetamodel_EReference),
-      matchPattern (parseTransformation Class2Relational) cm [ClassMetamodel_toEObject c] = Some r.    
-Proof.
-  intros.
-  exists (hd (BuildRuleA nil (BuildGuardExpressionA 0) nil) (TransformationA_getRules (parseTransformation Class2Relational))).
-  unfold matchPattern.
-  reflexivity.
-Qed.
-
-Theorem All_classes_instantiate :
-  forall (cm : ClassModel) (c: Class),
-    exists (t: Table),
-      instantiatePattern (parseTransformation Class2Relational) cm [ClassMetamodel_toEObject c] = Some [RelationalMetamodel_toEObject t].
-Proof.
-  intros.
-  exists (BuildTable (getClassId c) (getClassName c)).
-  unfold instantiatePattern.
-  unfold instantiateRuleOnPattern.
-  reflexivity.
-Qed.
-
-Theorem Concrete_attributes_instantiate :
-  forall (cm : ClassModel) (a: Attribute),
-    exists (c: Column), getAttributeDerived a=false -> 
-      instantiatePattern (parseTransformation Class2Relational) cm [ClassMetamodel_toEObject a] = Some [RelationalMetamodel_toEObject c].
-Proof.
-  intros.
-  exists (BuildColumn (getAttributeId a) (getAttributeName a)).
-  intros.
-  unfold instantiatePattern.
-  unfold instantiateRuleOnPattern.
-  simpl.
-  rewrite H.
-  reflexivity.
-Qed.
-
-Compute maxArity (parseTransformation Class2Relational).
+(* Print Class2Relational. *)
+(* Check Class2Relational. *)
